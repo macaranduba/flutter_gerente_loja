e9e63a21a1d15ebf1ddb79c6cda59d57d2b3ed17
José LR Reis <jreis78pt@gmail.com>
2021-04-23 20:14:21 -0300

    Listener de Usuário

--- lib/blocs/login_bloc.dart
+++ lib/blocs/login_bloc.dart
@@ -1,23 +1,56 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:gerente_loja/validators/login_validators.dart';
 import 'package:rxdart/rxdart.dart';
+
+enum LoginState {IDLE, LOADING, SUCCESS, FAIL}
 
 class LoginBloc extends BlocBase with LoginValidators {
   final _emailController = BehaviorSubject<String>();
   final _passController = BehaviorSubject<String>();
+  final _stateController = BehaviorSubject<LoginState>();
 
   Stream<String> get outEmail => _emailController.stream.transform(validateEmail);
   Stream<String> get outPass => _passController.stream.transform(validadePass);
+  // combine only emits if all streams have already emitted at least one value each
   Stream<bool> get outSubmittedValid => Rx.combineLatest2(outEmail, outPass,
-     (a, b) => a != null && b != null) ; // combine only emits if all stream have already emitted on value each
+     (a, b) => a != null && b != null
+  );
+  Stream<LoginState> get outState => _stateController.stream;
 
   Function(String) get changeEmail => _emailController.sink.add;
   Function(String) get changePass => _passController.sink.add;
+
+  LoginBloc() {
+    FirebaseAuth.instance.onAuthStateChanged.listen((firebaseUser) {
+      if(firebaseUser != null) {
+        print('logou!');
+        FirebaseAuth.instance.signOut();
+      } else {
+
+        _stateController.add(LoginState.IDLE);
+      }
+    });
+  }
+
+  void submit() {
+    final email = _emailController.value;
+    final pass = _passController.value;
+    _stateController.add(LoginState.LOADING);
+
+    FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: email, password: pass
+    ).catchError( (e) {
+      _stateController.add(LoginState.FAIL);
+    });
+  }
 
   @override
   void dispose() {
     _emailController.close();
     _passController.close();
+    _stateController.close();
+    
     super.dispose();
   }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -48,7 +48,8 @@ class _LoginScreenState extends State<LoginScreen> {
                       return SizedBox( // just to set the button's height
                         child: ElevatedButton(
                           child: Text('Entrar'),
-                          onPressed: snapshot.hasData && snapshot.data ? () {} : null,
+                          onPressed: snapshot.hasData && snapshot.data ?
+                            _loginBloc.submit : null,
                           style: ElevatedButton.styleFrom(
                             onSurface: Colors.pinkAccent.withAlpha(140), // disabled color
                             primary: Colors.pinkAccent,
